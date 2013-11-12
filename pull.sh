86bb6ebe6514bdf534f34fe64cb9a5d619a863ee
admin@dosomething.org <dosomething@admin.dosomething.org>
2013-11-12 19:20:53 +0000

    Once more.

--- pull.sh
+++ pull.sh
@@ -5,7 +5,7 @@ source ~/.bash_profile
 NEW_COMMIT="$1"
 OLD_COMMIT="$2"
 
-git stash && git pull --rebase
+git stash  # && git pull --rebase
 
 files=""
 for i in $(git diff $NEW_COMMIT $OLD_COMMIT --name-only)
