75dd41ae00ab898f1bc6970eaef0c27dce57eea2
admin@dosomething.org <dosomething@admin.dosomething.org>
2013-11-12 16:15:54 +0000

    Fixes for the pull script.

--- pull.sh
+++ pull.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+source ~/.bash_profile
+
 NEW_COMMIT="$1"
 OLD_COMMIT="$2"
 
+files=""
 for i in $(git diff $NEW_COMMIT $OLD_COMMIT --name-only)
 do
   git checkout $NEW_COMMIT $i
+  files="$files $i"
 done
+
+codercs --report=json $files
+echo "DONE"
 
